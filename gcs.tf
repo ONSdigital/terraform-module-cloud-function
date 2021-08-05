61d3d56f60f5d4d6581433a22b7c02eafe93e125
Samiwel Thomas <samiwel.thomas@gmail.com>
2021-08-05 16:07:21 +0100

    Add project to source bucket name.

--- gcs.tf
+++ gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "source_bucket" {
-  name                        = "source-${var.name}-bucket"
+  name                        = "source-${var.project != "" ? "${var.project}-" : ""}${var.name}-bucket"
   force_destroy               = true
   location                    = upper(var.region)
   uniform_bucket_level_access = true
